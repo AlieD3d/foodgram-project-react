5f1c16dbdccb54560f1ad43abecd4fdd8f9ac959
Arteom Nesterow <utchi@DESKTOP-0LSV1VH>
2022-12-03 22:53:34 +0300

    fixed bug backend

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -12,7 +12,7 @@ router.register('recipes', RecipeViewSet, 'recipes')
 router.register('users', UserViewSet, 'users')
 
 urlpatterns = (
-    path('auth/', include('djoser.urls.authtoken')),
-    path('', include('djoser.urls')),
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+
 )
